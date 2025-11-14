27e20d5a53af54b1b9123e4b77ca711abb01126d
VaNiesha Honani <vhonani77@gmail.com>
2025-11-13 19:33:26 -0800

    roman numeral python assignment

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,3 +1,25 @@
 def to_roman(num):
-    # write your code here!
-    pass
+    # write you
+    output = ''
+    roman_numeral_to_arabic = {
+        "I": 1,
+        "IV": 4,
+        "V": 5,
+        "IX": 9,
+        "X": 10,
+        "XL": 40,
+        "L": 50,
+        "C": 100,
+        "CD": 400,
+        "D": 500,
+        "CM": 900,
+        "M": 1000
+    }
+
+    for k, v in reversed(roman_numeral_to_arabic.items()):
+        evenly_divisible = num//v
+        if evenly_divisible >=1:
+            output += k * evenly_divisible
+            num -= v * evenly_divisible
+
+    return output
